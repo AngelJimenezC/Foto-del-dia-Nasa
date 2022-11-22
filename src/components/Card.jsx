3ab7ca39af589c389f680176aca2a0b5f06688cf
AngelJimenezC <105087736+AngelJimenezC@users.noreply.github.com>
2022-11-22 14:34:39 -0600

    ApiNasaFoto

---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+const Card = ({ items, isLoading }) => {
+
+    if (isLoading) {
+        return (
+            <span class="loader"></span>
+        )
+    } else {
+
+        return (
+            <div className='container mb-5'>
+                <div className="row mt-5 ">
+                    <div className="col-12">
+                        <div className="card" >
+                            <div className="card-header text-center">
+                                <b>{items.title}</b>
+                            </div>
+                            <img src={items.hdurl} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">By: {items.copyright ? items.copyright : 'NA'}</h5>
+                                <p className="card-text">{items.explanation}</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className='container'>
+            <h3 className='mt-5'> NASA Astronomical Picture of the Day</h3>
+            <p className='mb-5'>Desarrollado por: Angel Jimenez Cortes</p>
+            <h5>API: <a href="https://api.nasa.gov/planetary/apod?api_key=6w8aI1d8nauLsW5qvXJ0WaWOkjb7gTvkl7987Sga" target="_blank">https://api.nasa.gov/planetary/apod</a></h5>
+        </div>
+    )
+}
+
+export default Header
